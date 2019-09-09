28be2c6919ef8c95873679f96a2f95f802052f38
Camilo Cunha de Azevedo <camilotk@gmail.com>
2019-09-09 03:10:09 -0300

    last commit today

--- result.php
+++ result.php
@@ -1,8 +1,37 @@
 <?php include_once('template/header.php'); ?>
 
 <div class="page">
-    <div class="sidebar">
-        
+    <div class="row">
+        <div class="sidebar col-sm-4">
+
+        </div>
+        <div class="candidate-info col-sm-7">
+            <h2 class="nome-completo">Nome Completo</h2>
+            <p>Endereço residencial</p>
+
+            <h3>Resumo</h3>
+            <p class="resumo">
+                <?php
+                for ($i=0; $i < 250; $i++) { 
+                    echo 'a';
+                }
+                ?>
+            </p>
+
+            <h3>Experiência</h3>
+            <div class="experiencia-card">
+                <strong>Local</strong>
+                <p>descrição</p>
+                data inicio - data fim
+                <p>local</p>
+            </div>
+
+            <h3>Formação Acadêmica</h3>
+            <div class="experiencia-card">
+                <strong>Intituição</strong>
+                <p>local (data inicio - data fim)</p>
+            </div>
+        </div>
     </div>
 </div>
 
